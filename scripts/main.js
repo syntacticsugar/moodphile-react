3cb427ae03da80c09e33788bc7c5be008f99b777
Saerom Hong <hello@romcreative.com>
2016-01-16 18:34:33 -0500

    Adds rest of files, starts building MoodPusher component

--- scripts/main.js
+++ scripts/main.js
@@ -12,3 +12,36 @@ var h = require("./helpers");
 
 //Firebase
 var Rebase = require("re-base");
+
+var App = React.createClass({
+  getInitialState : function() {
+    return {
+      people : {},
+      moods : {}
+    }
+  },
+  render : function() {
+    return (
+    <section className="post-mood-wrapper col-md-4">
+      <div className="form-group">
+        <label className="control-label" for="inputDefault">Name</label>
+        <input type="text" className="form-control" id="inputDefault" placeholder="start typing..."/>
+      </div>
+      <div className="form-group">
+        <label className="control-label" for="inputDefault">What are you doing?</label>
+        <input type="text" className="form-control" id="inputDefault" placeholder="...like reading in bed"/>
+      </div>
+      <div className="form-group">
+        <label className="control-label" for="inputDefault">Location?</label>
+        <input type="text" className="form-control" id="inputDefault" placeholder="...like living room"/>
+      </div>
+      <div className="mood-pusher-wrapper">
+        <MoodPusher/>
+      </div>
+    </section>
+  )
+  }
+
+})
+
+ReactDOM.render(<App/>, document.querySelector('#main-moods'));
