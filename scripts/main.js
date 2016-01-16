fa4c1bc856e6fc13dbf35546e4c97c872800b4d2
Saerom Hong <hello@romcreative.com>
2016-01-16 15:13:55 -0500

    Adds scripts

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var Navigation = ReactRouter.Navigation;
+var History = ReactRouter.History;
+
+var createBrowserHistory = require("history/lib/createBrowserHistory");
+var h = require("./helpers");
+
+//Firebase
+var Rebase = require("re-base");
