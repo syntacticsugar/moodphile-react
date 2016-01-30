2e8a6f8d34901bdfb1b7d2e6a69cc7f13a0b3ad6
Saerom Hong <hello@romcreative.com>
2016-01-30 18:32:11 -0500

    Adds font awesome

---
+++ scripts/mood_attributes.js
@@ -0,0 +1,14 @@
+var mood = {
+        moodValue : this.refs.moodValue.value,
+        user : this.refs.user.value,
+        location : this.refs.location.value,
+        locationLarger : this.refs.locationLarger.value,
+        longDescription : this.refs.longDescription.value,
+        image : this.refs.image.value,
+        likes : this.refs.likes.value,
+        comments : this.refs.comments.value,
+        hoursOfSleep : this.refs.hoursOfSleep.value,
+        femaleCycle : this.refs.femaleCycle.value,
+        medication : this.refs.medication.value,
+        fitness : this.refs.fitness.value
+};
