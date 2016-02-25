b3b5e76ca75ca9016e8e96779d4dff4be609188a
Saerom Hong <hello@romcreative.com>
2016-02-25 16:04:05 -0500

    Adds social-media-service specific icon function

--- scripts/main.js
+++ scripts/main.js
@@ -488,22 +488,28 @@ var LoginWithSocialMedia = React.createClass({
   render : function() {
     var loggedInWith = this.props.loggedInWith;
     var authenticate = this.props.authenticate;
+    function iconSpecificToSocial(loggedInWith) {
+      return "fa fa-2x fa-" + loggedInWith;
+    }
 
     if (!loggedInWith) {
       return (
         <div className="login-with-social-media">
           <p>Log in securely:</p>
           <ul>
-            <li><a href="" onClick={function(e) {e.preventDefault(); authenticate('facebook')}}><i className="fa fa-facebook-square fa-2x"></i></a></li>
-            <li><a href="" onClick={function(e) {e.preventDefault(); authenticate('github')}}><i className="fa fa-github fa-2x"></i></a></li>
-            <li><a href=""><i className="fa fa-twitter fa-2x"></i></a></li>
+            <li><a href="" onClick={function(e) {e.preventDefault(); authenticate('facebook')}}>
+                    <i className="fa fa-facebook-square fa-2x"></i></a></li>
+            <li><a href="" onClick={function(e) {e.preventDefault(); authenticate('github')}}>
+                    <i className="fa fa-github fa-2x"></i></a></li>
+            <li><a href="" onClick={function(e) {e.preventDefault(); authenticate('twitter')}}>
+                    <i className="fa fa-twitter fa-2x"></i></a></li>
           </ul>
         </div>
       )
     } else {
       return (
-        <div>
-          You are logged in via {loggedInWith}
+        <div className="login-with-social-media">
+          You are logged in via <i className={iconSpecificToSocial(loggedInWith)}></i>{loggedInWith}
           <button className='btn'  onClick={this.props.logout}>Logout</button>
         </div>
       )
