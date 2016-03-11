092d43ab97ee4c2f0171c08221de9f9196dd4d00
Saerom Hong <hello@romcreative.com>
2016-03-10 23:15:39 -0500

    Pretty formats date in graph using xScale='time'

--- scripts/components/MoodDataViz.js
+++ scripts/components/MoodDataViz.js
@@ -2,10 +2,13 @@
 import React from 'react';
 //var ReactD3 = require('react-d3-tooltip');
 var Chart = require('react-d3-core').Chart;
+//var D3TimeFormat = require("d3-time-format");
 //var LineTooltip = require('react-d3-tooltip').LineTooltip;
 var LineTooltip = require('react-d3-basic').LineChart;
+var ScatterPlot = require('react-d3-basic').ScatterPlot;
 console.log(Chart);
 console.log(LineTooltip);
+console.log(ScatterPlot);
 
 
 var MoodDataViz = React.createClass({
@@ -18,13 +21,6 @@ var MoodDataViz = React.createClass({
     // field: is what field your data want to be selected
     // name: the name of the field that display in legend
     // color: what color is the line
-    var chartSeries = [
-      {
-        field: 'BMI',
-        name: 'BMI kthnxbye',
-        color: 'deeppink'
-      }
-    ];
     var moodChartSeries = [
       {
         field : "moodValue",
@@ -33,77 +29,27 @@ var MoodDataViz = React.createClass({
 
       }
     ];
+
     // your x accessor
     //var x = function(d) {
     //  return d.index;
     //};
+
+    //var formatTime = D3TimeFormat.timeFormat("%B %d, %Y");
+    //var parseTime = D3TimeFormat.timeFormat("%B %d, %Y");
+
+    //console.log("formatTime:");
+    //console.log(formatTime);
     var x = function(d) {
       return (new Date(d.submitTime)).getTime();
+      //return formatTime((new Date(d.submitTime)).getTime());
     };
     var moodChartData = Object.keys(this.props.moods).map ( function(key){
       return this.props.moods[key];
     }.bind(this));
-    var chartData =
-    [
-      {
-        name: "Lavon Hilll I",
-        BMI: 20.57,
-        age: 12,
-        birthday: "1994-10-26T00:00:00.000Z",
-        city: "Annatown",
-        married: true,
-        index: 1
-      },
-      {
-        name: "Jennifer Wilson",
-        BMI: 20,
-        age: 28,
-        birthday: "1987-02-09T00:00:00.000Z",
-        city: "West Virginia",
-        married: false,
-        index: 2
-      },
-      {
-        name: "Clovis Pagac",
-        BMI: 24.28,
-        age: 26,
-        birthday: "1995-11-10T00:00:00.000Z",
-        city: "South Eldredtown",
-        married: false,
-        index: 3
-      },
-      {
-        name: "Lucy Fong",
-        BMI: 19.9,
-        age: 26,
-        birthday: "1989-08-09T00:00:00.000Z",
-        city: "Brooklyn",
-        married: false,
-        index: 4
-      },
-      {
-        name: "Gaylord Paucek",
-        BMI: 24.41,
-        age: 30,
-        birthday: "1975-06-12T00:00:00.000Z",
-        city: "Koeppchester",
-        married: true,
-        index: 5
-      },
-      {
-        name: "Ashlynn Kuhn MD",
-        BMI: 23.77,
-        age: 32,
-        birthday: "1985-08-09T00:00:00.000Z",
-        city: "West Josiemouth",
-        married: false,
-        index: 6
-      },
-    ];
     return (
       <div className="container">
-
-        <LineTooltip
+        <ScatterPlot
           margins= {margins}
           title={title}
           //data={chartData}
@@ -113,6 +59,7 @@ var MoodDataViz = React.createClass({
           chartSeries={moodChartSeries}
           //chartSeries={chartSeries}
           x={x}
+          xScale={'time'}
         />
       </div>
     )
